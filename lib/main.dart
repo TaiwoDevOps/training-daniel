e2d864908c697590338cff84c79a7421075a0c65
Taiwo Adisa <adisataiwojoshua@gmail.com>
2023-02-10 21:33:06 +0100

    added network image profile

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class IdCardHome extends StatelessWidget {
         child: Container(
           height: 150,
           decoration: const BoxDecoration(
+            // image: DecorationImage(image: AssetImage("../assets/image/pp.png")),
             color: Color(0xFFD9D9D9),
             borderRadius: BorderRadius.only(
               bottomLeft: Radius.circular(20),
@@ -68,18 +69,18 @@ class IdCardHome extends StatelessWidget {
             child: Container(
               height: 317,
               width: 268,
+              clipBehavior: Clip.hardEdge,
               decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: NetworkImage(
+                    "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+                  ),
+                  fit: BoxFit.cover,
+                  alignment: Alignment.topCenter,
+                ),
                 shape: BoxShape.circle,
                 border: Border.all(
                   width: 15,
-                ),
-              ),
-              child: const Center(
-                child: Text(
-                  "Profile picture",
-                  style: TextStyle(
-                    fontSize: 32,
-                  ),
                 ),
               ),
             ),
@@ -87,6 +88,11 @@ class IdCardHome extends StatelessWidget {
           const SizedBox(
             height: 30,
           ),
+
+          //TODO: let the staff name be dynamic
+          //1. open the ID card generator app and enter the Company name, company email, staff ID, staff position, staff name and staff image
+          //2. Generate an ID card based on the previously entered staff name
+          //3. Change the profile picture based on the previously entered staff name
           Container(
             margin: const EdgeInsets.symmetric(horizontal: 50),
             width: double.infinity,
